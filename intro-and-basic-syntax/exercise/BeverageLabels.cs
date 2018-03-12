b544841604fa87023ecbd695c7ece581b814ccfe
swetllana <svetli.0805@gmail.com>
2018-03-12 22:06:05 +0200

    Documented the code of Solution of Beverage Labels

--- intro-and-basic-syntax/exercise/BeverageLabels.cs
+++ intro-and-basic-syntax/exercise/BeverageLabels.cs
@@ -4,11 +4,15 @@ public class BeverageLabels
 {
 	public static void Main()
 	{
+		// read the name of the product -> string
                 string name = Console.ReadLine();
+		// read the volume -> int
                 int volume = int.Parse(Console.ReadLine());
+		// read the energy of the product -> int
                 int energy = int.Parse(Console.ReadLine());
+		// read the sugar ->int
                 int sugar = int.Parse(Console.ReadLine());
-                
+                // write on the screen
                 System.Console.WriteLine("{0}ml {1}:\n{2}kcal, {3}g sugars",volume,name,(volume*energy)/100.00,(sugar*volume)/100.00);
 	}
 }
