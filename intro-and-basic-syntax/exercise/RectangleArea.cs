48490c30b1fff2ba12208265b33ecd159524ce3a
irenka <vivace@bav.bg>
2018-03-12 20:24:07 +0100

    added new task solution from Irena

---
+++ intro-and-basic-syntax/exercise/RectangleArea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class RecArea
+{
+    static void Main(string[] args)
+    {
+        float width = float.Parse(Console.ReadLine());
+        float depth = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{(width * depth):f2}");
+    }
+}
+
