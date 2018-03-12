ec16abb22e4de5516d34f94195a934ce2c7ca582
Peyo Peev <ppeev@mi9retail.com>
2018-03-12 21:43:00 +0200

    Documenting the code of the solution of Rectangle Area

--- intro-and-basic-syntax/exercise/RectangleArea.cs
+++ intro-and-basic-syntax/exercise/RectangleArea.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
-
 class RecArea
 {
     static void Main(string[] args)
     {
+	//Receiving the width from the console -> float
         float width = float.Parse(Console.ReadLine());
+	//Receiving the depth from the console -> float
         float depth = float.Parse(Console.ReadLine());
 
+	/* In the next line we calculate the 
+	 * rectangle area with the formula
+	 * width multiplied by depth
+	 * then we fix the point to the second zero */ 
         Console.WriteLine($"{(width * depth):f2}");
     }
 }
