e13c9786c4e63d1b71a71249308c583302aeaef4
irenka <vivace@bav.bg>
2018-03-13 21:29:09 +0100

    added comments on task "MilesToKilometers"

--- intro-and-basic-syntax/exercise/MilesToKilometers.cs
+++ intro-and-basic-syntax/exercise/MilesToKilometers.cs
@@ -4,8 +4,10 @@ public class MilesToKilometers
 {
 	public static void Main()
 	{
+        //Receiving miles, that need to be converted
 		decimal miles = decimal.Parse(Console.ReadLine());
 
+        //Printing the calculated conversion - miles->kilometres
 		System.Console.WriteLine("{0:0.00}",miles*1.60934m);
 	}
 }
