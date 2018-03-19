427118634ec1c1f8605b04285a34ff1864d015ab
Dimitar <jerry_900@abv.bg>
2018-03-19 22:06:32 +0200

    adding comments to DebitCardNumber

--- intro-and-basic-syntax/exercise/DebitCardNumber.cs
+++ intro-and-basic-syntax/exercise/DebitCardNumber.cs
@@ -5,11 +5,13 @@ public class DebitCardNumber
 {
 	public static void Main()
 	{
-		int oneNumber = int.Parse(Console.ReadLine());
+        // In the next four lines, we enter INTEGERES from the console
+        int oneNumber = int.Parse(Console.ReadLine());
 		int twoNumber = int.Parse(Console.ReadLine());
 		int threeNumber = int.Parse(Console.ReadLine());
 		int fourNumber = int.Parse(Console.ReadLine());
 
-		System.Console.WriteLine($"{oneNumber:D4} {twoNumber:D4} {threeNumber:D4} {fourNumber:D4}");
+        //After that we print the integers in one line and formatting them with "D4" in 4-digit debit card format
+        System.Console.WriteLine($"{oneNumber:D4} {twoNumber:D4} {threeNumber:D4} {fourNumber:D4}");
 	}
 }
